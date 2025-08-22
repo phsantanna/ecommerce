7a989f722b60f3aef0d0b79d54480a99378ea41e
Paulo Henrique <paulohenriquescarrinho@gmail.com>
2025-08-21 23:55:14 -0300

    Alteração na anotação ControllerAdvice para RestControllerAdvice

--- src/main/java/com/example/demo/exceptions/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/RestExceptionHandler.java
@@ -2,11 +2,11 @@ package com.example.demo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 
